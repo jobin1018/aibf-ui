de3f82c35936134e59bf16b4adde499474db6a52
jobin.joseph <jobin.joseph@scrapehero.com>
2025-01-16 09:51:56 +0530

    fixed build error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
@@ -94,4 +94,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
