8437678a183c13e8734127222b394f3b35405d36
jobin.joseph <jobin.joseph@scrapehero.com>
2025-03-26 17:27:36 +0530

    updated bibin email

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -3,7 +3,7 @@ export const ALLOWED_EMAILS = [
   "jobinjoseph1018@gmail.com",
   "johnylizy@gmail.com",
   "ben.phil205@gmail.com",
-  "bibinarpil2002@yahoo.com",
+  "bibinarpil2002@gmail.com",
 ];
 
 export const hasDashboardAccess = (): boolean => {
